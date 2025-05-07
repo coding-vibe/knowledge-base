b4f2b2e67ef14bd26884f270f7a86e6a4e930b4e
Ihor Sokolov <sihorjs@gmail.com>
2025-05-07 11:18:18 +0300

    add store

---
+++ code-samples/counter-slice.ts
@@ -0,0 +1,24 @@
+// Setup slice
+import { createSlice } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
+  },
+});
+
+export const { increment } = counterSlice.actions;
+
+export default counterSlice.reducer;
+
+// Setup store
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counterSlice';
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
