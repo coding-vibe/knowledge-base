3abbf57d1c4303c7a13c5e9b7ddd5c16f9b57fac
Ihor Sokolov <sihorjs@gmail.com>
2025-05-14 16:54:03 +0300

    add recursion sample

---
+++ code-samples/recursion.ts
@@ -0,0 +1,8 @@
+function getTagsCount(element) {
+  let childrenCount = element.children.length;
+  for (let i = 0; i < element.children.length; i++) {
+    childrenCount += getTagsCount(element.children[i]);
+  }
+
+  return childrenCount;
+}
